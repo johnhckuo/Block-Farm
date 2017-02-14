e73f54a32d40f446d6023190a6d15b10233dd906
johnhckuo <johnhckuo@gmail.com>
2017-02-14 18:32:39 +0800

    register form added

--- app/client/main.js
+++ app/client/main.js
@@ -20,3 +20,27 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+Template.next.events({
+  'click button'(event, instance) {
+    var temp = document.getElementById("flipper");
+    temp.className += " flipperClicked";
+    alert(character)
+    if (character == "seller"){
+        document.getElementById("buyerInfo").style.display = "none";
+    }else{
+        document.getElementById("sellerInfo").style.display = "none";
+    }
+
+  },
+});
+
+
+Template.chooseCharacters.events({
+  'click button'(event, instance) {
+      document.getElementById("seller").setAttribute("class", "btn btn-default");
+      document.getElementById("buyer").setAttribute("class", "btn btn-default");
+      event.target.className = "btn btn-info";
+      character = event.target.value;
+  },
+});
