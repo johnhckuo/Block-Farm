3ac827b06c74e0beb11c7fbf45e1e1d10a0b3a20
johnhckuo <johnhckuo@gmail.com>
2017-03-07 15:50:55 +0800

    manage page added

--- app/client/main.js
+++ app/client/main.js
@@ -20,7 +20,7 @@ import './main.html';
 //     instance.counter.set(instance.counter.get() + 1);
 //   },
 // });
-
+var stakeholderLength;
 
 Template.init.rendered = function() {
     if(!this._rendered) {
@@ -40,8 +40,43 @@ Template.init.rendered = function() {
     }
 }
 
+var hex2a = function(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
 if (Meteor.isClient) {
-  Template.body.events({
+  Template.manage.helpers({
+
+    stakeholders: function(){
+      var stakeholdersData = [];
+      var stakeholderLength = CongressInstance.getStakeholdersLength.call({from:web3.eth.accounts[0]}).c[0];
+      console.log(stakeholderLength)
+      for (var i = 0 ; i < stakeholderLength; i++){
+        var data = CongressInstance.getStakeholder.call(i, {from:web3.eth.accounts[0]});
+        //console.log(data);
+        stakeholdersData.push({
+          "name": hex2a(data[0]),
+          "threshold": data[1],
+          "fund": data[2],
+          "rate" :data[3],
+          "address" :data[4],
+          "since" : data[5],
+          "character": hex2a(data[6])
+        });
+        console.log(data);
+
+      }
+      return stakeholdersData;
+
+    }
+  });
+
+
+  Template.index.events({
     'click #arrow-down': function (e) {
         e.preventDefault();
         var height = window.innerHeight
@@ -57,10 +92,12 @@ if (Meteor.isClient) {
         character = event.target.value;
     },
     'click #previous': function (event){
+        event.preventDefault();
         var temp = document.getElementById("flipper");
         temp.className = " flipper";
     },
     'click #submit': function (event){
+        event.preventDefault();
         var name = $(".p_Name").val();
         var count = parseInt($(".p_Count").val());
         var accessStakeholders = $(".p_AccessStakeholders").val();
@@ -75,6 +112,7 @@ if (Meteor.isClient) {
 
     },
     'click #next': function (event){
+        event.preventDefault();
         var name = $(".s_Name").val();
         var threshold = parseInt($(".s_Threshold").val());
         var fund = parseInt($(".s_Fund").val());
