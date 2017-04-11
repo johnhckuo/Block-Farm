7301e4339c53587b9e2f847ccdf4b948f3410ecf
Calhoun5 <johnhckuo@gmail.com>
2017-04-11 15:24:11 +0800

    file structure updated

--- app/client/main.js
+++ app/client/main.js
@@ -2,6 +2,9 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './index.html';
+import './game.html';
+
 import './graph.js';
 import './mainActivity.js';
 
